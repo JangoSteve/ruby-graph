5ab70c75218750862d767cafbd9362f5a5f3ad8b
Steve Schwartz <steve@alfajango.com>
2012-12-20 01:24:30 -0500

    Refactor into logical sections with headings/explanations

--- graph.rb
+++ graph.rb
@@ -1,3 +1,6 @@
+# -----------------
+# READ DATA
+# -----------------
 data = <<-EOC
   Jan 1
   Feb 19
@@ -13,20 +16,33 @@ data = <<-EOC
   Dec 6
 EOC
 
-data_array = data.split(/\n/).map(&:strip).reject{ |s| s.nil? || s == "" }.map{ |s| s.split(/\s/) }
+# -----------------
+# CONFIG
+# -----------------
+HEIGHT = 20
+
+# -----------------
+# CALCULATIONS
+# -----------------
+
+# Break into array, e.g. [["Jan", 1], ["Feb", 19], etc]
+data_array = data.split(/\n/).reject{ |s| s.nil? || s == "" }.map{ |s| s.strip.split(/\s+/) }
+
+# Create hash to work with, e.g. {"Jan" => 1, "Feb" => 2, etc}
 data_hash = Hash[data_array].inject(Hash.new){ |h, a| h[a[0]] = a[1].to_i; h }
 
 max = data_hash.values.max
-
-height = 20
 
 # Calculate scaled values
 scaled_hash = data_hash.inject(Hash.new){ |h, a| h[a[0]] = (max.to_f / 20 * a[1]).round; h }
 
 axis_length = data_array.map{ |a| a[0] }.inject(0) { |i, key, value| i += (key.length + 1) } + 1
 
-height.times do |i|
-  this_line = height - i
+# -----------------
+# PRINT GRAPH
+# -----------------
+HEIGHT.times do |i|
+  this_line = HEIGHT - i
   keys_this_line = scaled_hash.select{ |k, v| v == this_line }.keys
 
   print "|"
